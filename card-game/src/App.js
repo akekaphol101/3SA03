33a05dc8b324136c178a97e2aa09324f562b918e
akekaphol101 <akekaphol101@gmail.com>
2019-09-22 22:20:54 +0700

    0.3

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
-const word = "Hello";
+
+const word = ['HOME','WORK','LAB','ADVANCE','HARD'];
+let it = word[Math.floor(Math.random()*word.length)];
+
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state ={requstAns : false }
+      }
+
+      
  render() {
  return (
  <div>
